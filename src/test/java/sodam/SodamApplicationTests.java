d87bbb11a9bd64a37560a8b2ba1c52caa4649a09
김가영 <yeong.kmj@gmail.com>
2024-08-27 01:27:47 +0900

    chore: test file

--- src/test/java/sodam/SodamApplicationTests.java
+++ src/test/java/sodam/SodamApplicationTests.java
@@ -3,7 +3,7 @@ package sodam;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SodamApplicationTests {
 
 	@Test
